90ab4e4e6697e1b088068155b91614c06f52e13d
PURBOMITA <purbomita242-35-720@diu.edu.bd>
2026-08-18 04:21:36 +0600

    Initialize storage and add page navigation

---
+++ script.js
@@ -0,0 +1,47 @@
+let reports = JSON.parse(localStorage.getItem("campusFix")) || [];
+
+
+/* PAGE SWITCH */
+
+function showPage(page, button) {
+
+    document.querySelectorAll(".page").forEach(p => {
+        p.classList.remove("active");
+    });
+
+    document.getElementById(page).classList.add("active");
+
+
+    document.querySelectorAll(".nav").forEach(n => {
+        n.classList.remove("active");
+    });
+
+    if (button) {
+        button.classList.add("active");
+    }
+
+
+    if (page === "dashboard") {
+        pageTitle.innerText = "Dashboard";
+        pageText.innerText =
+            "Monitor problems reported around campus.";
+    }
+
+    if (page === "report") {
+        pageTitle.innerText = "Report Problem";
+        pageText.innerText =
+            "Submit a new problem to the campus team.";
+    }
+
+    if (page === "reports") {
+        pageTitle.innerText = "My Reports";
+        pageText.innerText =
+            "Track the problems you have reported.";
+        displayReports();
+    }
+
+    updateDashboard();
+}
+
+
+/* ADD REPORT */
